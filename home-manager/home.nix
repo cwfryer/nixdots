6082dbc3d943e1ddb955a006e53ffcb2b557aacc
Casey Fryer <casey@extrafries.org>
2024-02-17 22:16:58 -0800

    update 2024.02.16

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -39,6 +39,7 @@
       allowUnfree = true;
       # Workaround for https://github.com/nix-community/home-manager/issues/2942
       allowUnfreePredicate = _: true;
+      permittedInsecurePackages = ["electron-25.9.0"];
     };
   };
 
